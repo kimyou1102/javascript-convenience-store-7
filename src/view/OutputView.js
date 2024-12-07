7a924c1c4bad7f9a91c19a3e9035a9f4dc709bb3
kimyou1102 <yoosk5485@naver.com>
2024-12-08 00:21:11 +0900

    feat: 재고 출력 메서드 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -6,4 +6,23 @@ export default class OutputView {
     Console.print('현재 보유하고 있는 상품입니다.\n');
     this.printInventoryList(inventory);
   }
+
+  static printInventoryList(inventory) {
+    inventory.forEach(({ name, price, quantity, promotion }) => {
+      const money = price.toLocaleString('ko-KR');
+      let quantityWithUnit = `${quantity}개`;
+
+      if (quantity === 0) quantityWithUnit = '재고 없음';
+
+      this.printInventory(name, money, promotion, quantityWithUnit);
+    });
+  }
+
+  static printInventory(name, money, promotion, quantityWithUnit) {
+    if (promotion === 'null') {
+      Console.print(`- ${name} ${money}원 ${quantityWithUnit}`);
+      return;
+    }
+    Console.print(`- ${name} ${money}원 ${quantityWithUnit} ${promotion}`);
+  }
 }
