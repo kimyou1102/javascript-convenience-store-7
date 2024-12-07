bc55ab9289827e4c21db21f5f52b28b120e79fe1
kimyou1102 <yoosk5485@naver.com>
2024-12-08 00:22:55 +0900

    feat: 구입할 상품과 수를 입력받는 기능 구현
    - 입력받은 문자열을 객체로 변환해주는 유틸함수 생성

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,6 +1,8 @@
+import InputView from '../view/InputView.js';
 import OutputView from '../view/OutputView.js';
 import InventoryManagement from '../model/InventoryManagement.js';
 import { getProductsData, getPromotionsData } from '../utils/getfileData.js';
+import { parseProducts } from '../utils/parseProduct.js';
 
 export default class Controller {
   async start() {
@@ -9,5 +11,13 @@ export default class Controller {
     const inventoryManagement = new InventoryManagement(productsData, promotionsData);
     const inventory = inventoryManagement.getInventoryInfo();
     OutputView.printGreetingAndInventory(inventory);
+    const buyProducts = await this.getBuyProducts();
+  }
+
+  async getBuyProducts() {
+    const input = await InputView.getInput(
+      '\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])',
+    );
+    return parseProducts(input);
   }
 }
---
+++ src/view/InputView.js
@@ -0,0 +1,11 @@
+import { Console } from '@woowacourse/mission-utils';
+
+export default class InputView {
+  static async getInput(message) {
+    try {
+      return await Console.readLineAsync(`${message}\n`);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
